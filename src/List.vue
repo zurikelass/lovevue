5d388e8d9dbbd8a6d4a1c9e6a2671a570c823b29
zurikelass <zuragogberashvili93@gmail.com>
2023-01-25 09:40:56 +0400

    ცვლადებისთვის შევქმენით ფუქნცია

--- src/List.vue
+++ src/List.vue
@@ -3,20 +3,25 @@ export default{
     props:{
         data:{ type:Array, required:true}
     },
+    data() {return {description: ''} }, //შევქმენით ფუნქცია ცვლადებისთვის
     methods:{
       eventHandler: function(e){
         e.preventDefault()
         console.log(e.target.innerHTML, e.target.getAttribute('href'))
+        this.description=e.target.getAttribute('href') //რომელი დესქრიფშენიც არის მას მიემართოს მხლოდ
       }
     },
 }
 </script>
 <template>
     <ul>
+
         <li  v-for="item in data" :key="item.id"> 
             <a :href="item.description" @click.pervant="eventHandler">{{ item.title }} </a></li>
             
     </ul>
+    <!-- დესქრიფშენი გამოდის დინამიურად -->
+    <div>{{ description }}</div>    
 </template>
 
 <style scoped> 
