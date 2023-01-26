01a4ef5855bd67e7ac8c42df586bdeb452bf288d
zurikelass <zuragogberashvili93@gmail.com>
2023-01-26 06:01:54 +0400

    მართკუთხედი

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,54 @@
 <script>
-import List from './List.vue'
-  export default{
-    components:{
-      List
+
+export default {
+  data() {
+    return {
+      width: 20,
+      height: 30,
+      simagle: null,
+      show: false
+    }
+  },
+  computed: {
+    calculateSpace(){
+      return this.width * this.height
     },
-    data(){
-      return{
-        pageTitle:"My first vue",
-        data: [
-         {
-          id:1,
-          title: "Menu",
-          url:"https://devcamp.com/trails/44/campsites/281/guides/working-links-vue-js",
-          description:"This is Main Menu"
-         },
-         {
-          id:2,
-          title: "Contact",
-          url:"https://devcamp.com/trails/44/campsites/281/guides/working-links-vue-js",
-          description:"This is contact",
-         } ,
-        {
-          id:3,
-          title: "About Us",
-          url:"https://devcamp.com/trails/44/campsites/281/guides/working-links-vue-js",
-          description: "This is About us",
-        },
-        ]
-      }
-    
-    } 
-    
+    calculateMoculoba(){
+      return this.calculateSpace * this.simagle
+    },
+    calculateForm1(){
+      return this.calculateSpace
+
+    }
   }
+}
+
 </script>
+
 <template>
-  <h1>{{pageTitle}}</h1>
-  <List :data="data"/>
+  <div>
+    <form>
+
+      <label>სიგრძე</label><br />
+      <input v-model="width" /><br />
+
+      <label>სიგანე</label><br />
+      <input v-model="height" /><br />
+      
+      <label>სიმაღლე</label><br />
+      <input v-model="simagle" /><br />
+    </form>
+    <br />
+    <button @click.prevent="show = !show">
+      <span v-if="!show">Show</span>
+      <span v-if="show">Hide</span>
+    </button>
+    <p v-if="show">ფართობი: {{ calculateSpace }}</p>
+    <p v-if="show">მოცულობა: {{ calculateMoculoba }}</p>
+  </div>
+
+  <div class="Form1">
+    
+  </div>
 </template>
+ 
