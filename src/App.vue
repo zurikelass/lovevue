094bf6dfe8da42655f5b594423fb84dded63aee5
zurikelass <zuragogberashvili93@gmail.com>
2023-01-31 06:02:52 +0400

    ვერ გავიგე რა მეშლება

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,80 @@
 <script>
-import './assets/base.css'
-import TodoList from './TodoList.vue'
-import TodoItemForm from './components/TodoItemForm.vue'
+import axios from 'axios'
+
 export default {
-    components: {
-        TodoList,
-        TodoItemForm,
-    },
-    data() {
-        return {
-            todos: [
-                { id: 1, name: "to do item", done: false },
-                { id: 2, name: "to do item 2", done: false },
-                { id: 3, name: "to do item 3", done: true },
-            ]
-        }
-    },
-   methods: {
-    putNewItemTodoList(data){
-      this.todos.push({
-        id: this.nextId,
-        name: data,
-        done: false
-      })
-    },
-    changeStatus(record){
-      let v=this.find(item=>item.id===record.id)
-      v.done=!v.done
-    },
-    deleteItemFromTodoList(record){
-      let v=this.todos.find(item => item.id === record.id)
-      this.todos.splice(this.todos.indexOf(v),1)
+  data() {
+    return {
+      range: [5, 10, 15, 50],
+      limit: 5,
+      lang: 'ge',
+      apiUrl: 'http://items.magischer.de/api/products',
+      products: [],
+      res: null,
     }
-   },
-   computed: {
-    nextId(){
-      return this.todos.length + 1
-    },
-    activeTodos(){
-      return this.todos.filter(value=> value.done === false)
+  },
+  methods: {
+    getDataFromApiUrl(url = this.apiUrl) {
+      axios.get(url, {
+        params: {
+          limit: this.limit,
+          lang: this.lang,
+        },
+      }).then((response) => {
+        this.res = response.data
+        this.products = response.data.data
+      })
 
     },
-    doneTodos(){
-      return this.todos.filter(value=> value.done === true)
+    nextPage() {
+      this.getDataFromApiUrl(this.res?.next_page_url)
+    },
+    prevPage() {
+      this.getDataFromApiUrl(this.res?.prev_page_url)
+    },
+    firstPage() {
+      this.getDataFromApiUrl(this.res?.first_page_url)
+    },
+    lastPage() {
+      this.getDataFromApiUrl(this.res?.last_page_url)
+    },
+    limitChange(e) {
+      this.limit = e.target.value
+      this.getDataFromApiUrl()
     },
 
-   }
+  },
+  mounted() {
+    this.getDataFromApiUrl()
+  }
 }
 </script>
-     
+
 <template>
-     <h1 style="text-align: center;" class="hh">Our First Form <br/> With Vue.js</h1> <br/>
-  <div style="height: 100%; border-radius: 10px; width: 8cm; background-color: bisque; margin-right: 0px;" class="container">
-    <TodoItemForm id="TDF" @onSubmitCustom="putNewItemTodoList($event)"/>
-    <h2 id="H2">All Todo Items</h2>
-    <TodoList id="TDL" :data="todos"/>
-    <h2>Active Todos</h2>
-    <TodoList :changeDoneStatus="true" :data="activeTodos" @onChangeDoneStatus="ChangeStatus($event)"/>
-    <h2>ToDo With Done Status</h2>
-    <TodoList :canDelete="true" :data="doneTodos" @onDelete="deleteItemFromTodoList($event)" />
+  <select @change="limitChange">
+    <option v-for="amount in range" :key="amount.index" :value="amount">Per page: {{ amount }}</option>
+  </select>
+  <table class="table-auto" style="margin: 5px;">
+    <thead>
+      <tr>
+        <th>Product Id</th>
+        <th>Product Name</th>
+        <th>Cover Image</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in res.data" :key="item.index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }} </td>
+        <td>
+          <img style="width: 80px; height: auto;" :src="item.img.cover" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <div>
+    <a href="#" :disabled="!res?.first_page_url" @click.prevent="firstPage">First page</a>
+    <button :disabled="!res?.prev_page_url" @click.prevent="prevPage">Previous</button>
+    <button :disabled="!res?.next_page_url" @click.prevent="nextPage">Next</button>
+    <a href="#" :disabled="!res?.last_page_url" @click.prevent="lastPage">Last Page</a>
   </div>
 </template>
